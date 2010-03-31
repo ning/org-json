61fe21110340142db1708dfabf856f6823b4aab5
Henning Schmiedehausen <henning@ning.com>
2010-03-30 18:26:21 -0700

    start converting the test class into unit test suite

---
+++ src/test/java/org/json/suite/Obj.java
@@ -0,0 +1,52 @@
+package org.json.suite;
+
+import org.json.JSONObject;
+import org.json.JSONString;
+
+/**
+ *  Obj is a typical class that implements JSONString. It also
+ *  provides some beanie methods that can be used to
+ *  construct a JSONObject. It also demonstrates constructing
+ *  a JSONObject with an array of names.
+ */
+class Obj implements JSONString {
+    public String aString;
+    public double aNumber;
+    public boolean aBoolean;
+
+    public Obj(String string, double n, boolean b) {
+        this.aString = string;
+        this.aNumber = n;
+        this.aBoolean = b;
+    }
+
+    public double getNumber() {
+        return this.aNumber;
+    }
+
+    public String getString() {
+        return this.aString;
+    }
+
+    public boolean isBoolean() {
+        return this.aBoolean;
+    }
+
+    public String getBENT() {
+        return "All uppercase key";
+    }
+
+    public String getX() {
+        return "x";
+    }
+
+    public String toJSONString() {
+        return "{" + JSONObject.quote(this.aString) + ":" +
+            JSONObject.doubleToString(this.aNumber) + "}";
+    }
+    @Override
+    public String toString() {
+        return this.getString() + " " + this.getNumber() + " " +
+            this.isBoolean() + "." + this.getBENT() + " " + this.getX();
+    }
+}
